9f0cb7d86949f47a5079494b47ef97c6a22cf502
ericwang184 <ehw001@ucsd.edu>
2023-01-29 22:37:01 -0800

    stuff

--- StringServer.java
+++ StringServer.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 class Handler implements URLHandler {
 
-    String strArray[] = new String[1];
+    String strArray[] = new String[0];
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/add")) {
+        if (url.getPath().equals("/add-message")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 String newStrArray[] = new String[strArray.length + 1];
@@ -16,21 +16,13 @@ class Handler implements URLHandler {
                 newStrArray[strArray.length] = parameters[1];
                 strArray = newStrArray;
             }
-            return parameters[1];
-        } else if (url.getPath().equals("/search")) {
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")){
-                String returnStrArray[] = new String[strArray.length];
-                for (int i = 0; i < strArray.length; i++){
-
-                }
-            }
-            
+           
         } 
+        return strArray.toString();
     }
 }
 
-class SearchEngine {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
